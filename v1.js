7e902b5bca2d2f08f2272e22adad26ff49c53635
monafraht <153129039+MONAfraht@users.noreply.github.com>
2023-12-07 00:29:05 +0200

    Add files via upload

---
+++ v1.js
@@ -0,0 +1,22 @@
+
+const navbar = document.querySelector('.navbar')
+ 
+   
+window.addEventListener("scroll",()=>{
+
+ navbar.classList.add('fixed')
+     if(scrollY > 58){
+  
+     }else{
+   navbar.classList.remove('fixed')
+ 
+
+
+}
+
+
+
+
+});
+
+
